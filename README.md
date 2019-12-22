8c0e0be936667b12dfa30605e67d6aaadc36b8e3
Daniel Lutz <daniel.lutz@outlook.com>
2019-12-22 21:00:28 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mklink2
+# SymLinkDir
 
+SymLinkDir is a simple CLI for creating symbol links with [Jeff Brown Junction Points class](https://www.codeproject.com/Articles/15633/Manipulating-NTFS-Junction-Points-in-NET).
+
+## License
+
+This project is licensed under the [MIT](LICENSE) License
---
+++ src/SymLinkDir/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SymLinkDir
+{
+    class SymLinkDir
+    {
+        static void Main(string[] args)
+        {
+            if (TryParse(args, out ParseResult parseResult))
+            {
+                if (mklink.Exists(parseResult.Link))
+                    mklink.Delete(parseResult.Link);
+
+                mklink.Create(parseResult.Link, parseResult.Target, false);
+
+            }
+        }
+
+        static bool TryParse(string[] args, out ParseResult parseResult)
+        {
+            parseResult = null;
+
+            if (args.Length != 2) return false;
+            if (!System.IO.Directory.Exists(args[1])) return false;
+
+            parseResult = new ParseResult { Link = args[0], Target = args[1] };
+            return true;
+        }
+    }
+
+    class ParseResult
+    {
+        public string Link { get; set; }
+
+        public string Target { get; set; }
+    }
+}
